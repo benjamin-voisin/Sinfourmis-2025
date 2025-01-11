65b8ed2656ece7aa111ae7784ed3741ff466afe8
Alexis Hamon <alexis.hamon@ens-rennes.fr>
2025-01-11 17:25:27 +0000

    retoru pp

--- src/fourmis/utils/utils.c
+++ src/fourmis/utils/utils.c
@@ -17,3 +17,12 @@ uint8_t random_other_dir(fourmi_etat *etat, const salle *salle) {
     }
     return dir;
 }
+
+void retour_pp(FILE* f, fourmi_retour ret) {
+    fprintf(f, "ACTION {\n", ret.action);
+    fprintf(f, "    action     = %u\n", ret.action);
+    fprintf(f, "    arg        = %u\n", ret.arg);
+    fprintf(f, "    fero_type  = %u\n", ret.depose_pheromone);
+    fprintf(f, "    fero       = %u\n", ret.pheromone);
+    fprintf(f, "}\n", ret.action);
+}
--- src/fourmis/utils/utils.h
+++ src/fourmis/utils/utils.h
@@ -2,6 +2,10 @@
 
 #include "../../sinfourmis.h"
 
+#include <stdio.h>
+
 uint8_t random_dir(fourmi_etat *etat, const salle *salle);
 
 uint8_t random_other_dir(fourmi_etat *etat, const salle *salle);
+
+void retour_pp(FILE* f, fourmi_retour ret);
