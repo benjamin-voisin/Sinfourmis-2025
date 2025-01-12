045c871522dec74556c3f15493838c59ceabf850
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2025-01-12 10:44:18 +0100

    ⚡️ remove useless PASS

--- src/queen/queen.cpp
+++ src/queen/queen.cpp
@@ -116,10 +116,8 @@ void reine_thread() {
 						size_t number_of_ants = 10;
 
 						for (size_t i = 0; i < number_of_ants; i++) {
-							queen_state._scheduler.add_task(Task(PASS, arg));
 							queen_state._scheduler.add_task(Task(CREER_MANGER, arg));
 							queen_state._scheduler.add_task(Task(GASLIGHT_MANGER, arg));
-							queen_state._scheduler.add_task(Task(PASS, arg));
 							queen_state._scheduler.add_task(Task(EAT_FORUMIS, arg));
 							queen_state._scheduler.add_task(Task(SEND_FORUMIS, arg));
 						}
