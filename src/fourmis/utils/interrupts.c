36bc3ed9396abdb5b2df815e81becee476d82e59
Alexis Hamon <alexis.hamon@ens-rennes.fr>
2025-01-12 00:00:54 +0000

    better interrupt const

--- src/fourmis/utils/interrupts.c
+++ src/fourmis/utils/interrupts.c
@@ -2,7 +2,7 @@
 #include "pile.h"
 
 enum fourminterrupt_e interrupt(fourmi_etat *etat, const salle *salle) {
-    if ((etat->eau < 16) && !(pile_vide(etat->memoire)))
+    if ((etat->eau < 19) && !(pile_vide(etat->memoire)))
         return INTERRUPT_WATER;
     if (salle->taille_liste > 1)
         return INTERRUPT_ENNEMY;
