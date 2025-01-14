06309c4f69871649acc053ffd1f6d0965e31fa8f
Alexis Hamon <alexis.hamon@ens-rennes.fr>
2025-01-14 12:50:53 +0000

    basics new reine api

---
+++ src/reine/api/basics.c
@@ -0,0 +1,57 @@
+#include "basics.h"
+
+reine_retour reine_action_appel(reine_action act, uint8_t arg) {
+    reine_retour ret;
+    assert(act <= REINE_PASSE);
+    ret.action = act;
+    ret.arg = arg;
+    return ret;
+}
+
+reine_retour reine_action_creer_fourmi(uint8_t arg) {
+    return reine_action_appel(CREER_FOURMI, arg);
+}
+
+reine_retour reine_action_envoyer_fourmi(uint8_t arg) {
+    return reine_action_appel(ENVOYER_FOURMI, arg);
+}
+
+reine_retour reine_action_recuperer_fourmi(uint8_t arg) {
+    return reine_action_appel(RECUPERER_FOURMI, arg);
+}
+
+reine_retour reine_action_ameliore_stockage() {
+    return reine_action_appel(AMELIORE_STOCKAGE, 0);
+}
+
+reine_retour reine_action_ameliore_production() {
+    return reine_action_appel(AMELIORE_PRODUCTION, 0);
+}
+
+reine_retour reine_action_ameliore_envoi() {
+    return reine_action_appel(AMELIORE_ENVOI, 0);
+}
+
+reine_retour reine_action_ameliore_vitesse_amelioration() {
+    return reine_action_appel(AMELIORE_VITESSE_AMELIORATION, 0);
+}
+
+reine_retour reine_action_ameliore_ramassage() {
+    return reine_action_appel(AMELIORE_RAMASSAGE, 0);
+}
+
+reine_retour reine_action_ameliore_vie() {
+    return reine_action_appel(AMELIORE_VIE, 0);
+}
+
+reine_retour reine_action_ameliore_eau() {
+    return reine_action_appel(AMELIORE_EAU, 0);
+}
+
+reine_retour reine_action_ameliore_degats() {
+    return reine_action_appel(AMELIORE_DEGATS, 0);
+}
+
+reine_retour reine_action_passe() {
+    return reine_action_appel(REINE_PASSE, 0);
+}   
---
+++ src/reine/api/basics.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../../sinfourmis.h"
+
+reine_retour reine_action_creer_fourmi(uint8_t arg);
+
+reine_retour reine_action_envoyer_fourmi(uint8_t arg);
+
+reine_retour reine_action_recuperer_fourmi(uint8_t arg);
+
+reine_retour reine_action_ameliore_stockage();
+
+reine_retour reine_action_ameliore_production();
+
+reine_retour reine_action_ameliore_envoi();
+
+reine_retour reine_action_ameliore_vitesse_amelioration();
+
+reine_retour reine_action_ameliore_ramassage();
+
+reine_retour reine_action_ameliore_vie();
+
+reine_retour reine_action_ameliore_eau();
+
+reine_retour reine_action_ameliore_degats();
+
+reine_retour reine_action_passe();
