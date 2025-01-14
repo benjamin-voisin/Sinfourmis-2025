441e696812294a77f2ea7ff3800c2051aba58b73
Alexis Hamon <alexis.hamon@ens-rennes.fr>
2025-01-14 18:09:40 +0000

    reine typing malloc

--- src/reine/reine.c
+++ src/reine/reine.c
@@ -4,7 +4,7 @@
 #include "../fourmis/utils/stats.h"
 
 reine_t* reine_alloc() {
-    reine_t* reine = Malloc(sizeof(reine_t));
+    reine_t* reine = (reine_t*) Malloc(sizeof(reine_t));
     reine->tick = 0;
     reine->stats = stats_init();
     return reine;
